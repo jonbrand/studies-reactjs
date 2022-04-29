d5120e6e3388a40f9555f1a824b96787accfcc1c
jonbrand <jonatas@grupoeureka.com.br>
2022-04-29 16:09:11 -0300

    refactor: add handleSubmit to contents

--- src/components/Forms/FormOneState/Form.constant.jsx
+++ src/components/Forms/FormOneState/Form.constant.jsx
@@ -1,1 +1,20 @@
-export const initialFormData = { name: '', email: '', password: '', country: 'Escolha seu país' };
+export const initialFormData = { 
+  name: '', 
+  email: '', 
+  password: '', 
+  country: 'Escolha seu país' 
+};
+
+export const handleSubmit = (e) => {
+  // previne comportamento padrao do formulario
+  e.preventDefault();
+  // pega os dados no form
+  const formData = new FormData(e.target);
+  // cria um objeto com os dados
+  const data = Object.fromEntries(formData)
+
+  const message = `Seu nome é: ${data.name} \nSeu e-mail é: ${data.email} \nSua senha é: ${data.password} \nSeu país é: ${data.country}`;
+
+  alert(message)
+  
+}
--- src/components/Forms/FormOneState/Form.jsx
+++ src/components/Forms/FormOneState/Form.jsx
@@ -1,32 +1,15 @@
 import { useState } from 'react';
-import { initialFormData } from './Form.constant';
-import { Container, Select, Button, Title, Formulary, Input } from './Form.styles'
+import { initialFormData, handleSubmit } from './Form.constant';
+import { Container, Select, Button, Title, Formulary, Input } from './Form.styles';
 
 export const Form = () => {
-
   const [inputValues, setInputValues] = useState(initialFormData);
-
-  console.log(inputValues)
 
   const handleInputChange = (e) => {
     // pega os valores do form
     const { name, value } = e.target;
-
     // seta os valures atualizados
     setInputValues({...inputValues, [name]: value });
-  }
-
-  const handleSubmit = (e) => {
-    // previne comportamento padrao do formulario
-    e.preventDefault();
-    // pega os dados no form
-    const formData = new FormData(e.target);
-    // cria um objeto com os dados
-    const data = Object.fromEntries(formData)
-
-    const message = `Seu nome é: ${data.name} \nSeu e-mail é: ${data.email} \nSua senha é: ${data.password} \nSeu país é: ${data.country}`;
-
-    alert(message)
   }
 
   return (
